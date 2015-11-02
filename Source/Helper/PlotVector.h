f8db7c7b8240e9b9701cb4c946416b1f230ca69b
Nige Li <nigel.li@qq.com>
2015-11-02 23:00:47 +0800

    Plot vector (channel shoule equal to 1). Flip the result, now the output image is right.

--- Source/Helper/PlotVector.h
+++ Source/Helper/PlotVector.h
@@ -8,7 +8,7 @@ using namespace cv;
 /*!
 Plot the vector (like a histogram).
 
-@param src [in] Mat, input vector, its rows or cols should equal to 1.
+@param src [in] Mat, input vector, neither its rows or cols should equal to 1.
 @param dst [out] Mat, output single channel image.
 @param size [in] Size, output image size, 800w and 600h as default.
 @return int, 0x0000 = Success.
